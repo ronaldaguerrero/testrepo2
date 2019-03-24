8ef30bece8090e93f5522937d7c610ce0dd8c20b
Ronald Guerrero <ronald.a.guerrero@gmail.com>
2019-03-23 23:12:59 -0700

    first commit

---
+++ MemoryMatch/js_practice.js
@@ -0,0 +1,36 @@
+// var myGreeting = "Locked out of the elevator";
+// // variable declaration = variable assignment
+
+// console.log(myGreeting);
+// // print statement (variable to print)
+
+// var myNumber = 13;
+// myNumber = myNumber * 13;
+// console.log(myNumber);
+
+// myGreeting = 62;
+// console.log(myGreeting);
+
+// Boolean
+// var myNumber = 13;
+// if(myNumber < 100){
+//     console.log('Less than 100');
+// }
+
+// var myNumber = prompt();
+// if (myNumber > 0){
+//     console.log('Positive');
+// }
+// else if (myNumber < 0){
+//     console.log('Negative');
+// }
+// else if (myNumber == 13){
+//     console.log('Unlucky');
+// }
+
+for (var i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+
+
